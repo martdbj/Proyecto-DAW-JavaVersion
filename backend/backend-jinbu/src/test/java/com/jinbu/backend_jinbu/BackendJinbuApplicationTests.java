28908ea30e42613d91ee2fe403702ad069ff2bbb
martdbj <181728462+martdbj@users.noreply.github.com>
2025-11-16 12:32:52 +0100

    Añadido estructura Maven para el backend

---
+++ backend/backend-jinbu/src/test/java/com/jinbu/backend_jinbu/BackendJinbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinbu.backend_jinbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJinbuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
